400115323cc18fea22fbacfc42f0cef02b347913
Huangbingxiao <767788035@qq.com>
2018-06-01 13:10:11 +0800

    233
    更新

--- src/DatabaseAccess.java
+++ src/DatabaseAccess.java
@@ -8,78 +8,64 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-@WebServlet("/DatabaseAccess")
 public class DatabaseAccess extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    // JDBC 驱动名及数据库 URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-    static final String DB_URL = "jdbc:mysql://localhost:3306/music";
+    static final String DB_URL = "jdbc:mysql://120.79.89.4:3306/music";
     
     // 数据库的用户名与密码，需要根据自己的设置
     static final String USER = "guest";
     static final String PASS = "Rao614614614@"; 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+
     public DatabaseAccess() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Connection conn = null;
         Statement stmt = null;
-        // 设置响应内容类型
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        String title = "Servlet Mysql 测试 - 菜鸟教程";
-        String docType = "<!DOCTYPE html>\n";
-        out.println(docType +
-        "<html>\n" +
-        "<head><title>" + title + "</title></head>\n" +
-        "<body bgcolor=\"#f0f0f0\">\n" +
-        "<h1 align=\"center\">" + title + "</h1>\n");
+//        String title = "Servlet Mysql 测试 - 菜鸟教程";
+//        String docType = "<!DOCTYPE html>\n";
+//        out.println(docType +
+//        "<html>\n" +
+//        "<head><title>" + title + "</title></head>\n" +
+//        "<body bgcolor=\"#f0f0f0\">\n" +
+//        "<h1 align=\"center\">" + title + "</h1>\n");
         try{
-            // 注册 JDBC 驱动器
             Class.forName("com.mysql.jdbc.Driver");
             
-            // 打开一个连接
             conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-            // 执行 SQL 查询
+            System.out.println(conn);
             stmt = conn.createStatement();
+            
             String sql;
-            sql = "SELECT email FROM email";
+            sql = "SELECT email FROM user_info";
             ResultSet rs = stmt.executeQuery(sql);
-
-            // 展开结果集数据库
+            System.out.println(rs);
             while(rs.next()){
-                // 通过字段检索
-               
+   
                 String email = rs.getString("email");
-    
-                // 输出数据
+            	System.out.println(rs.getString("email"));
+           
                 out.println("ID: " + email);
 
             }
-            out.println("</body></html>");
+//            out.println("</body></html>");
 
-            // 完成后关闭
             rs.close();
             stmt.close();
             conn.close();
         } catch(SQLException se) {
-            // 处理 JDBC 错误
+          
             se.printStackTrace();
         } catch(Exception e) {
-            // 处理 Class.forName 错误
+         
             e.printStackTrace();
         }finally{
-            // 最后是用于关闭资源的块
+       
             try{
                 if(stmt!=null)
                 stmt.close();
@@ -95,11 +81,8 @@ public class DatabaseAccess extends HttpServlet {
        
     }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO Auto-generated method stub
+      
         doGet(request, response);
     }
 }
